8da0166f60ab0ff9679ade5d00c8b4fe1f64dcf7
olankri59 <83861585+olankri59@users.noreply.github.com>
2021-07-17 23:44:18 +0700

    Add files via upload

--- Plusnumber.js
+++ Plusnumber.js
@@ -5,9 +5,9 @@ function printPairs(arr, sum)
     for (let i = 0; i < n; i++)
         for (let j = i + 1; j < n; j++)
             if (arr[i] + arr[j] === sum)
-                 console.log("(" + arr[i] + ", " + arr[j] + ")");
+                console.log("(" + arr[i] + ", " + arr[j] + ")");
 }
- 
+
 let arr = [ 1, 2, 3, 4, 5 ];
 let sum = 7;
 printPairs(arr, sum);
